b58213f4f3c0cea4aa7f9ee2cf1b44055fa23ce4
omolaso <adextee001@gmail.com>
2023-06-23 10:19:40 +0100

    first commit

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ const password = document.getElementById("password");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
   if (!nameField.value || !email.value || !password.value) return;
 
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  numbers.map((number) => {
-    if (nameField.value.includes(number)) {
-      return;
-    }
-  });
+  if (Number.isInteger(nameField.value)) return;
+
+  if (!email.value.endsWith(".com")) return;
 
   finalSubmit();
 });
